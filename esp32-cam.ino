b34d5e37059711c76f759a5bf9003e0d4bbbaba7
Vikram Alagh <61557646+sixthkrum@users.noreply.github.com>
2021-12-18 03:00:42 +0530

    removed un-needed code to reduce used space

--- esp32-cam.ino
+++ esp32-cam.ino
@@ -11,15 +11,6 @@ sensor_t * sens;
 
 WebServer server(80);
 
-const char HEADER[] = "HTTP/1.1 200 OK\r\n" \
-                      "Access-Control-Allow-Origin: *\r\n" \
-                      "Content-Type: multipart/x-mixed-replace; boundary=123456789000000000000987654321\r\n";
-const char BOUNDARY[] = "\r\n--123456789000000000000987654321\r\n";
-const char CTNTTYPE[] = "Content-Type: image/jpeg\r\nContent-Length: ";
-const int hdrLen = strlen(HEADER);
-const int bdrLen = strlen(BOUNDARY);
-const int cntLen = strlen(CTNTTYPE);
-
 const char JHEADER[] = "HTTP/1.1 200 OK\r\n" \
                        "Content-disposition: inline; filename=capture.jpg\r\n" \
                        "Content-type: image/jpeg\r\n\r\n";
